17833961827b60e5df7b5dcc81a0809897e9352f
nishapandey1 <np930400@gmail.com>
2023-09-29 21:30:50 +0545

    basic python

---
+++ area.py
@@ -0,0 +1,10 @@
+# length= int (input("enter the lenth "))
+# breadth= int (input("enter the breadth "))
+# area=print(f" area = {length * breadth}")
+# perimeter=print(f"perimeter ={2*(length + breadth)}")
+
+length=10
+breadth=20
+area= length*breadth
+peri=2*(length+breadth)
+print("area", area, "perimeter", peri,)
---
+++ file.py
@@ -0,0 +1,7 @@
+num1= int(input("enter a number"))
+num2= int(input("enter another number"))
+add= num1 + num2
+sub= num1 - num2
+multi= num1 * num2
+div= num1 / num2
+print(f"add = {add} \n  sub = {sub} \n multi = {multi} \n div = {div}")
