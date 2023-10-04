cead16fe177a8f0e932c25f11e2684238aa664dd
nishapandey1 <np930400@gmail.com>
2023-10-04 09:50:58 +0545

    basic loop

---
+++ loop.py
@@ -0,0 +1,20 @@
+'''Wap to print the text "Hello" for 10 times.'''
+# for i in range(10):
+#     print("hello")
+
+'''Wap to print 1 to 100'''
+# for i in range (1, 101, 1):
+#     print (i)
+
+'''WAP to print form 50 to 100'''
+# for i in range (50, 9, -1):
+#     print (i)
+
+"""WAP to print even numbers from 10 to 41"""
+# for i in range (10, 42, 2):
+#     print (i)
+
+"""WAP to print odd numbers from 100 to 50"""
+for i in range (100, 50,-1):
+    if (i%2!=0):
+        print(i,end=",")
