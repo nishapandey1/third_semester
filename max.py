e246c6af361231945a06a881e4b222f20a5da03e
nishapandey1 <np930400@gmail.com>
2023-11-27 08:22:38 +0545

    file-handling

--- max.py
+++
@@ -1,8 +0,0 @@
-num1= int (input("enter a number "))
-num2= int (input("enter another number "))
-if num1 > num2:
-    print("the maximum number is:", num1)
-elif num2 >num1:
-    print("the maximum number is", num2)
-else:
-    print("two numbers are equal")
--- min.py
+++
@@ -1,6 +0,0 @@
-num1= int (input("enter a number "))
-num2= int (input("enter another number "))
-if num1 > num2:
-    print("the minimum number is:", num2)
-else:
-    print("the minimum number is", num1)
